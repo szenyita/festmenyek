83acf1d47f5075392200830575c022eef59a5ae5
szenyita <szenyita27@gmail.com>
2024-10-02 23:09:36 +0200

    Order v2

--- src/app/penztar/page.tsx
+++ src/app/penztar/page.tsx
@@ -3,7 +3,7 @@ import SzemelyesAdatok from "@/components/SzemelyesAdatok";
 export default function page() {
   return (
     <div className="flex items-center justify-center pb-16">
-      <SzemelyesAdatok />;
+      <SzemelyesAdatok />
     </div>
   );
 }
--- src/lib/rendelesKezeles.tsx
+++ src/lib/rendelesKezeles.tsx
@@ -4,60 +4,48 @@ import prisma from "./prisma";
 import { revalidatePath } from "next/cache";
 
 export async function addOrder(formData: FormData) {
-  const data: { [key: string]: any } = {};
-
   const felhasznaloId = formData.get("felhasznaloId") as string;
 
   const vezeteknev = formData.get("vezeteknev") as string;
-  data.vezeteknev = vezeteknev;
 
   const keresztnev = formData.get("keresztnev") as string;
-  data.keresztnev = keresztnev;
 
   const telefonszam = formData.get("telefonszam") as string;
-  data.telefonszam = telefonszam;
 
   const varos = formData.get("varos") as string;
-  data.varos = varos;
 
   const iranyitoszamFn = formData.get("iranyitoszam") as string;
   const iranyitoszam = parseInt(iranyitoszamFn);
-  data.iranyitoszam = iranyitoszam;
 
   const utca = formData.get("utca") as string;
-  data.utca = utca;
 
   const hazszamFn = formData.get("hazszam") as string;
   const hazszam = parseInt(hazszamFn);
-  data.hazszam = hazszam;
 
   const emeletFn = formData.get("emelet") as string;
   const emelet = parseInt(emeletFn);
-  if (emelet) {
-    data.emelet = emelet;
-  }
 
   const ajtoFn = formData.get("ajto") as string;
   const ajto = parseInt(ajtoFn);
-  if (ajto) {
-    data.ajto = ajto;
-  }
 
   const csengoFn = formData.get("csengo") as string;
   const csengo = parseInt(csengoFn);
-  if (csengo) {
-    data.csengo = csengo;
-  }
 
   const festmenyIds = formData.getAll("festmenyIds") as string[];
-  if (festmenyIds.length === 0) {
-    return { errorMessage: "At least one painting must be selected" };
-  }
 
   try {
     await prisma.rendeles.create({
       data: {
-        ...data,
+        vezeteknev,
+        keresztnev,
+        telefonszam,
+        varos,
+        iranyitoszam,
+        utca,
+        hazszam,
+        emelet: emelet || null,
+        ajto: ajto || null,
+        csengo: csengo || null,
         felhasznalo: { connect: { felhasznaloId } },
         festmenyek: {
           connect: festmenyIds.map((id) => ({ festmenyId: id })),
