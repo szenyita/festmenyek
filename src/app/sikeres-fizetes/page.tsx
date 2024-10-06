cc8fa716c0c418d5aa328f73cb90b212fe755cf8
szenyita <szenyita27@gmail.com>
2024-10-06 15:07:38 +0200

    Stripe v0

--- src/app/sikeres-fizetes/page.tsx
+++ src/app/sikeres-fizetes/page.tsx
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-09-30.acacia",
+});
 
 export default async function page({
   searchParams,
